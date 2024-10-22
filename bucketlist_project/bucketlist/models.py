51d51f531eda8a9ba997ae2b8a7acadc19e0f28f
Zamzamke <zamzamchelimo@gmail.com>
2024-10-22 10:18:21 +0300

    create new bucketlist item model

--- bucketlist_project/bucketlist/models.py
+++ bucketlist_project/bucketlist/models.py
@@ -1,8 +1,7 @@
 from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
-
+#Bucketlist models
 class BucketList(models.Model):
     name = models.CharField(max_length=255)
     description = models.TextField(blank=True, null=True)
@@ -12,3 +11,28 @@ class BucketList(models.Model):
 
     def __str__(self):
         return self.name
+
+
+ #Models to categorize items
+class BucketListItem(models.Model):
+    CATEGORY_CHOICES = (
+        ('travel', 'Travel'),
+        ('career', 'Career'),
+        ('personal', 'Personal'),
+    )
+    
+    STATUS_CHOICES = (
+        ('pending', 'Pending'),
+        ('completed', 'Completed'),
+    )
+
+    bucketlist = models.ForeignKey(BucketList, related_name="items", on_delete=models.CASCADE)
+    title = models.CharField(max_length=255)
+    description = models.TextField(null=True, blank=True)
+    category = models.CharField(max_length=50, choices=CATEGORY_CHOICES, default='personal')
+    priority = models.IntegerField(default=0)  # You can set this to range from 1 (high) to 5 (low)
+    status = models.CharField(max_length=50, choices=STATUS_CHOICES, default='pending')
+
+    def __str__(self):
+        return self.title
+ 
